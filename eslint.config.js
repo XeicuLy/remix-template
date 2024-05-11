e8b23e73506e2645b336da4133a30535ce49d0e6
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-05-12 01:10:25 +0900

    🔧 : プロジェクト全体の設定

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,28 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+
 /**
  * @type {import('eslint').Linter.FlatConfig[]}
  */
 export default [
   {
     ignores: ["node_modules", ".cache", "build", "public/build", ".env"],
+  },
+  {
+    ...eslint.configs.recommended,
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.commonjs,
+        ...globals.es5,
+      },
+    },
   },
 ];
