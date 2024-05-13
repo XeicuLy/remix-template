42c9271c07615c06006cd11935df0b4985ea87b9
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-05-13 22:42:45 +0900

    🔧 : 主にReactのESLintを更新

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,10 @@ import globals from "globals";
 import eslint from "@eslint/js";
 import tsEslint from "typescript-eslint";
 import tsParser from "@typescript-eslint/parser";
-import pluginReact from "eslint-plugin-react";
+import reactPlugin from "eslint-plugin-react";
+import reactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 /**
@@ -13,10 +16,8 @@ export default tsEslint.config(
     ignores: ["node_modules", ".cache", "build", "public/build", ".env"],
   },
   // 全体の設定
-  eslint.configs.recommended,
-  ...tsEslint.configs.recommended,
-  eslintConfigPrettier,
   {
+    extends: [eslint.configs.recommended, ...tsEslint.configs.recommended],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -36,19 +37,46 @@ export default tsEslint.config(
   // Reactの設定
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
+    ...reactRecommended,
+    ...reactJSXRuntime,
     plugins: {
-      react: pluginReact,
+      react: reactPlugin,
+      ["jsx-a11y"]: jsxA11yPlugin,
+    },
+    languageOptions: {
+      ...reactRecommended.languageOptions,
+      ...reactJSXRuntime.languageOptions,
     },
     rules: {},
+    extends: [],
+    settings: {
+      react: {
+        version: "detect",
+      },
+      formComponents: ["Form"],
+      linkComponents: [
+        { name: "Link", linkAttribute: "to" },
+        { name: "NavLink", linkAttribute: "to" },
+      ],
+      "import/resolver": {
+        typescript: {},
+      },
+    },
   },
   // TypeScriptの設定
   {
     files: ["**/*.{ts,tsx}"],
+    extends: [],
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         project: "./tsconfig.json",
       },
     },
+  },
+  // Prettierの設定
+  {
+    extends: [eslintConfigPrettier],
+    rules: {},
   }
 );
