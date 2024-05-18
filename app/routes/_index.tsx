e9290d0763b47c24bad7549de7e06c4aba6ceb9f
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-05-18 23:24:49 +0900

    🎨 : TailwindCSS適用

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -7,7 +7,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
+      <h1 className='text-3xl font-bold underline'>Welcome to Remix</h1>
       <ul>
         <li>
           <a target='_blank' href='https://remix.run/tutorials/blog' rel='noreferrer'>
